769b4325cb878c5e44bfcf95337ce3bbf557edef
Pablo Soria Ferrer <10505140@ieselgrao.org>
2021-04-16 08:34:02 +0200

    Prueba Push

--- Hola.java
+++ Hola.java
@@ -2,5 +2,6 @@ public class Hola {
     public static void main(String[] args){
         System.out.println("Hola");
         System.out.println("Que tal");
+	System.out.println("Hello GIT");
     }
 }
